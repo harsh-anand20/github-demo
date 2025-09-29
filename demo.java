d0c8ed1b99abcf3482b4ad464e61f7ab0d5e3da2
Harsh Anand <harsh.anand20999@gmail.com>
2025-09-30 02:23:03 +0530

    First commit for Demo

---
+++ demo.java
@@ -0,0 +1,12 @@
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("Hello Puchkuu!");
+        helperFunction(3000);
+    }
+
+    static void helperFunction(int param) {
+        // This is a helper function
+        System.out.println("I love you: " + param);
+    }
+}
